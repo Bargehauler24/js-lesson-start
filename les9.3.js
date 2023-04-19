7377b4a0967b9d37a95144ac326aea7c1efd7dc1
i.burlachenkov <i.burlachenkov@mail.ru>
2023-04-19 09:31:05 +0300

    lesson 9

---
+++ les9.3.js
@@ -0,0 +1,10 @@
+const arr = ['!', 'JS', 'люблю', 'Я'];
+const resultArray = [];
+
+for(let i = arr.length - 1; i >= 0; i --) {
+	resultArray.push(arr[i]);
+}
+
+console.log(resultArray.join (' '));
+
+console.log(arr.reverse().join(' '));
---
+++ les9.6.js
@@ -0,0 +1,13 @@
+const arr = [1,4,8,7, 'test'];
+
+for (let i =0; i < arr.length; i++){
+	console.log(arr[i]);
+}
+
+for (let element of arr) {
+	console.log(element);
+}
+
+for (let index in arr) {
+	console.log(arr[index]);
+}
